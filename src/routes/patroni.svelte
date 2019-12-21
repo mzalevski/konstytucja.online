d9db5ca6e915e930ff2ee9761b77bcbd8e211d23
Maciej Zalewski <mzalevski.dev@gmail.com>
2019-12-21 22:41:58 +0100

    add list of patrons to patroni subpage

--- src/routes/patroni.svelte
+++ src/routes/patroni.svelte
@@ -9,23 +9,37 @@
   h3 {
     padding-top: 16px;
   }
-
+  .low-opacity {
+    color: rgba(0, 0, 0, 0.3);
+  }
 </style>
 
 <svelte:head>
   <title>Patroni</title>
-  <meta name="description" content="Informacje o czytniku Konstytucji Rzeczypospolitej Polskiej z dnia 2 kwietnia 1997 r.">
-  <meta name="keywords" content="konstytucja, informacje, info, konstytucjarp, konstytucjaonline, online, prawo, konstytucja art, trybunał konstytucyjny, sądownictwo, trybunał, prezydent, rada ministrów, sejm, senat">
-  <meta name="konstytucja" content="website">
+  <meta
+    name="description"
+    content="Informacje o czytniku Konstytucji Rzeczypospolitej Polskiej z dnia
+    2 kwietnia 1997 r." />
+  <meta
+    name="keywords"
+    content="konstytucja, informacje, info, konstytucjarp, konstytucjaonline,
+    online, prawo, konstytucja art, trybunał konstytucyjny, sądownictwo,
+    trybunał, prezydent, rada ministrów, sejm, senat" />
+  <meta name="konstytucja" content="website" />
 </svelte:head>
 
-<Nav segment={'info'}/>
+<Nav segment={'info'} />
 
 <div in:fly={{ y: 100, duration: 1000 }}>
-  <h4>
-    Prace w toku... 🧰🔨
-  </h4>
-  <h3>Patroni</h3>
+  <h3>Lista wspierających projekt przez portal <a href="https://patronite.pl/maciejzalewski">Patronite</a>:</h3>
+  <h3>Mecenasi 💎</h3>
+  <p class="low-opacity">brak</p>
+  <h3>Patroni 3.0 🥇</h3>
+  <p class="low-opacity">brak</p>
+  <h3>Patroni 2.0 🥈</h3>
+  <p class="low-opacity">brak</p>
+  <h3>Patroni 1.0 🥉</h3>
+  <p class="low-opacity">brak</p>
 </div>
 
 <Footer />
