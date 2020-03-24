880a135b5bead9311d9923cc56bac3e58b076680
mzalevski <mzalevski.dev@gmail.com>
2020-03-24 23:36:01 +0100

    make font smaller

--- src/routes/preambula.svelte
+++ src/routes/preambula.svelte
@@ -33,129 +33,129 @@
       <div class="p-1" in:fly={{ y: 200, duration: 800 }}>
         <p
           class="text-center text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           W trosce o byt i przyszłość naszej Ojczyzny,
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           odzyskawszy w 1989 roku możliwość suwerennego i demokratycznego
           stanowienia o Jej losie,
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           my, Naród Polski - wszyscy obywatele Rzeczypospolitej,
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           zarówno wierzący w Boga będącego źródłem prawdy, sprawiedliwości,
           dobra i piękna,
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           jak i nie podzielający tej wiary,
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           a te uniwersalne wartości wywodzący z innych źródeł,
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           równi w prawach i w powinnościach wobec dobra wspólnego - Polski,
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           wdzięczni naszym przodkom za ich pracę, za walkę o niepodległość
           okupioną ogromnymi ofiarami, za kulturę zakorzenioną w chrześcijańskim
           dziedzictwie Narodu i ogólnoludzkich wartościach,
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           nawiązując do najlepszych tradycji Pierwszej i Drugiej
           Rzeczypospolitej,
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           zobowiązani, by przekazać przyszłym pokoleniom wszystko, co cenne z
           ponad tysiącletniego dorobku,
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           złączeni więzami wspólnoty z naszymi rodakami rozsianymi po świecie,
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           świadomi potrzeby współpracy ze wszystkimi krajami dla dobra Rodziny
           Ludzkiej,
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           pomni gorzkich doświadczeń z czasów, gdy podstawowe wolności i prawa
           człowieka były w naszej Ojczyźnie łamane,
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           pragnąc na zawsze zagwarantować prawa obywatelskie, a działaniu
           instytucji publicznych zapewnić rzetelność i sprawność,
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           w poczuciu odpowiedzialności przed Bogiem lub przed własnym sumieniem,
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           ustanawiamy Konstytucję Rzeczypospolitej Polskiej
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           jako prawa podstawowe dla państwa
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           oparte na poszanowaniu wolności i sprawiedliwości, współdziałaniu
           władz, dialogu społecznym oraz na zasadzie pomocniczości
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           umacniającej uprawnienia obywateli i ich wspólnot.
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           Wszystkich, którzy dla dobra Trzeciej Rzeczypospolitej tę Konstytucję
           będą stosowali,
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           wzywamy, aby czynili to, dbając o zachowanie przyrodzonej godności
           człowieka,
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           jego prawa do wolności i obowiązku solidarności z innymi,
         </p>
         <p
           class="text-center pt-4 text-md sm:text-xl md:text-1xl lg:text-2xl
-          xl:text-4xl">
+          xl:text-3xl">
           a poszanowanie tych zasad mieli za niewzruszoną podstawę
           Rzeczypospolitej Polskiej.
         </p>
