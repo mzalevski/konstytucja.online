f3792a0f8b238da16a419a00af1f59250ee73229
mzalevski <mzalevski.dev@gmail.com>
2020-03-28 18:34:17 +0100

    fix postcss config to include svelte class:[n]={}

--- postcss.config.js
+++ postcss.config.js
@@ -6,7 +6,8 @@ module.exports = {
       content: [
         './src/**/*.svelte'
       ],
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g)
+        .map(el => el.includes('class:') ? el.substr('class:'.length) : el) || []
     })
   ]
 }
