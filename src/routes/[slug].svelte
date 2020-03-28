4604c5668ad5c87d7c5a7c1a3d1d9e4e89a4283d
mzalevski <mzalevski.dev@gmail.com>
2020-03-28 20:05:42 +0100

    style art page

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -61,14 +61,14 @@
 <Nav segment={'articles'} />
 
 <div class="flex justify-between">
-  <a class="back" rel="prefetch" href="#{$page.params.slug}">powrót</a>
+  <a class="w-16" rel="prefetch" href="#{$page.params.slug}">powrót</a>
   <h3 class="chapter-name">
     {article.chapter['id']} {article.chapter['title']}
   </h3>
-  <div class="flex justify-between">
+  <div class="flex w-16 justify-end">
     {#if $page.params.slug > 1}
       <a
-        class="left"
+        class="flex flex-col justify-center"
         rel="prefetch"
         href="/{parseInt($page.params.slug) - 1}"
         transition:fade={{ duration: 1000 }}
@@ -76,9 +76,11 @@
           isDescriptionVisible = false;
           isDisqusVisible = false;
         }}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512">
+        <svg
+          class="fill-current w-5 h-5 text-gray-900"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 256 512">
           <path
-            fill="currentColor"
             d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4
             24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7
             409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z" />
@@ -87,7 +89,7 @@
     {/if}
     {#if $page.params.slug < 243}
       <a
-        class="right"
+        class="flex flex-col justify-center"
         rel="prefetch"
         href="/{parseInt($page.params.slug) + 1}"
         transition:fade={{ duration: 1000 }}
@@ -95,9 +97,11 @@
           isDescriptionVisible = false;
           isDisqusVisible = false;
         }}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512">
+        <svg
+          class="fill-current w-5 h-5 text-gray-900"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 256 512">
           <path
-            fill="currentColor"
             d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9
             0l-22.6-22.6c-9.4-9.4-9.4-24.6
             0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4
@@ -108,47 +112,61 @@
   </div>
 </div>
 
-<h1>{article.title}</h1>
-<div class="art-body">
-  <div in:fade={{ duration: 1000 }} class="content no-dots">
+<h1
+  class="pt-8 sm:pt-10 md:pt-12 lg:pt-16 text-xl sm:text-4xl font-thin
+  text-center">
+  {article.title}
+</h1>
+<div class="py-4">
+  <div in:fade={{ duration: 1000 }} class="sm:text-lg text-justify">
     {@html article.html}
   </div>
-
-  {#if article.desc != ``}
-    <div class="content">
-      <button on:click={() => (isDescriptionVisible = !isDescriptionVisible)}>
-        {#if !isDescriptionVisible}
-          <span class="low-opacity">wyjaśnienie treści artykułu | pokaż</span>
-        {:else}
-          <span class="low-opacity">wyjaśnienie treści artykułu | schowaj</span>
+  <div class="mt-8 border-t border-gray-200">
+    {#if article.desc != ``}
+      <div class="pt-8">
+        <button on:click={() => (isDescriptionVisible = !isDescriptionVisible)}>
+          {#if !isDescriptionVisible}
+            <span class="pt-2 text-gray-600">
+              wyjaśnienie treści artykułu | pokaż
+            </span>
+          {:else}
+            <span class="pt-2 text-gray-600">
+              wyjaśnienie treści artykułu | schowaj
+            </span>
+          {/if}
+        </button>
+        {#if isDescriptionVisible}
+          <div
+            class="text-justify"
+            in:fly|fade={{ y: -40, duration: 1000 }}
+            out:fly|fade={{ y: -40, duration: 200 }}>
+            <h3>Wyjaśnienie treści artykułu</h3>
+            {@html article.desc}
+          </div>
         {/if}
-      </button>
-      {#if isDescriptionVisible}
-        <div
-          style="art-desc"
-          in:fade={{ duration: 1000 }}
-          out:fade={{ duration: 500 }}>
-          <h3>Wyjaśnienie treści artykułu</h3>
-          {@html article.desc}
-        </div>
+      </div>
+    {/if}
+
+    <div class="pt-4">
+      {#if !isDisqusVisible}
+        <button class="pt-2 text-gray-600" on:click={() => showDisqus()}>
+          dyskusja nad artykułem | pokaż
+        </button>
+      {:else}
+        <button
+          class="pt-2 text-gray-600"
+          on:click={() => (isDisqusVisible = false)}>
+          dyskusja nad artykułem | schowaj
+        </button>
       {/if}
     </div>
-  {/if}
 
-  {#if !isDisqusVisible}
-    <button class="low-opacity" on:click={() => showDisqus()}>
-      dyskusja nad artykułem | pokaż
-    </button>
-  {:else}
-    <button class="low-opacity" on:click={() => (isDisqusVisible = false)}>
-      dyskusja nad artykułem | schowaj
-    </button>
-  {/if}
-
-  {#if isDisqusVisible}
-    <div
-      id="disqus_thread"
-      in:fly={{ y: 200, duration: 1000 }}
-      out:fly={{ y: 100, duration: 500 }} />
-  {/if}
+    {#if isDisqusVisible}
+      <div
+        class="pt-4"
+        id="disqus_thread"
+        in:fly={{ y: -40, duration: 1000 }}
+        out:fly={{ y: -40, duration: 200 }} />
+    {/if}
+  </div>
 </div>
