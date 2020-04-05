3fc8fd4a02500c19470c3931d101f93dfe0656f1
mzalevski <mzalevski.dev@gmail.com>
2020-04-05 12:45:57 +0200

    rm heart.png & add svg heart

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,14 @@
 <footer class="flex justify-center py-4 mt-4 border-t border-gray-200">
   <p>
     z miłości
-    <img class="inline-block h-2 md:h-3" src="images/heart.png" alt="" />
+    <svg
+      class="inline w-3 text-red-600 fill-current md:w-4 "
+      viewBox="0 0 20 20">
+      <path
+        d="M17.19 4.155c-1.672-1.534-4.383-1.534-6.055 0L10 5.197 8.864
+        4.155c-1.672-1.534-4.382-1.534-6.054 0-1.881 1.727-1.881 4.52 0 6.246L10
+        17l7.19-6.599c1.88-1.726 1.88-4.52 0-6.246z" />
+    </svg>
     do Polski ~
     <a
       class="italic hover:text-red-new"
