8cda13e06dabe169ba7b30e6a3e50de66cd1dc72
mzalevski <mzalevski.dev@gmail.com>
2020-04-18 11:06:28 +0200

    rm cursor-help

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   theme: {
     extend: {
-      cursor: {
-        help: 'help'
-      },
       colors: {
         'red-new': '#a02828',
         'dark-overlay': 'rgba(0, 0, 0, .6)',
