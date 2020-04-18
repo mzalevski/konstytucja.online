189e3223571bedf2650c076e36210f3c7ae8db89
mzalevski <mzalevski.dev@gmail.com>
2020-04-18 21:54:55 +0200

    mk whitespace bigger

--- src/routes/preambula.svelte
+++ src/routes/preambula.svelte
@@ -32,102 +32,102 @@
       W trosce o byt i przyszłość naszej Ojczyzny,
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       odzyskawszy w 1989 roku możliwość suwerennego i demokratycznego
       stanowienia o Jej losie,
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       my, Naród Polski - wszyscy obywatele Rzeczypospolitej,
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       zarówno wierzący w Boga będącego źródłem prawdy, sprawiedliwości, dobra i
       piękna,
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       jak i nie podzielający tej wiary,
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       a te uniwersalne wartości wywodzący z innych źródeł,
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       równi w prawach i w powinnościach wobec dobra wspólnego - Polski,
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       wdzięczni naszym przodkom za ich pracę, za walkę o niepodległość okupioną
       ogromnymi ofiarami, za kulturę zakorzenioną w chrześcijańskim dziedzictwie
       Narodu i ogólnoludzkich wartościach,
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       nawiązując do najlepszych tradycji Pierwszej i Drugiej Rzeczypospolitej,
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       zobowiązani, by przekazać przyszłym pokoleniom wszystko, co cenne z ponad
       tysiącletniego dorobku,
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       złączeni więzami wspólnoty z naszymi rodakami rozsianymi po świecie,
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       świadomi potrzeby współpracy ze wszystkimi krajami dla dobra Rodziny
       Ludzkiej,
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       pomni gorzkich doświadczeń z czasów, gdy podstawowe wolności i prawa
       człowieka były w naszej Ojczyźnie łamane,
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       pragnąc na zawsze zagwarantować prawa obywatelskie, a działaniu instytucji
       publicznych zapewnić rzetelność i sprawność,
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       w poczuciu odpowiedzialności przed Bogiem lub przed własnym sumieniem,
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       ustanawiamy Konstytucję Rzeczypospolitej Polskiej
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       jako prawa podstawowe dla państwa
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       oparte na poszanowaniu wolności i sprawiedliwości, współdziałaniu władz,
       dialogu społecznym oraz na zasadzie pomocniczości
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       umacniającej uprawnienia obywateli i ich wspólnot.
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       Wszystkich, którzy dla dobra Trzeciej Rzeczypospolitej tę Konstytucję będą
       stosowali,
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       wzywamy, aby czynili to, dbając o zachowanie przyrodzonej godności
       człowieka,
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       jego prawa do wolności i obowiązku solidarności z innymi,
     </p>
     <p
-      class="pt-4 text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       a poszanowanie tych zasad mieli za niewzruszoną podstawę Rzeczypospolitej
       Polskiej.
     </p>
