7a008f6ac475abf3ecb15c4316976760c0e035d8
mzalevski <mzalevski.dev@gmail.com>
2020-04-22 13:43:12 +0200

    rm fullname & link

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,20 +2,20 @@
   <p>
     z miłości
     <svg
-      class="inline w-3 text-red-600 fill-current md:w-4 "
+      class="inline w-3 pb-px text-red-600 fill-current md:w-4"
       viewBox="0 0 20 20">
       <path
         d="M17.19 4.155c-1.672-1.534-4.383-1.534-6.055 0L10 5.197 8.864
         4.155c-1.672-1.534-4.382-1.534-6.054 0-1.881 1.727-1.881 4.52 0 6.246L10
         17l7.19-6.599c1.88-1.726 1.88-4.52 0-6.246z" />
     </svg>
-    do Polski ~
-    <a
+    do Polski
+    <!-- <a
       class="italic hover:text-red-new"
       href="https://maciej-zalewski.pl"
       rel="nofollow"
       target="_blank">
       Maciej Zalewski
-    </a>
+    </a> -->
   </p>
 </footer>
