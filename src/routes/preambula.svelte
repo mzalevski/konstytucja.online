fb50090bd0a98cdad7e4bba13f060e65635642d3
mzalevski <kontakt@mzalevski.pl>
2020-12-31 00:53:24 +0100

    qf

--- src/routes/preambula.svelte
+++ src/routes/preambula.svelte
@@ -1,6 +1,32 @@
 <script>
   import { fly, fade } from "svelte/transition";
   import Nav from "../components/Nav.svelte";
+
+  const paragraphs = [
+    "W trosce o byt i przyszłość naszej Ojczyzny,",
+    "odzyskawszy w 1989 roku możliwość suwerennego i demokratycznego stanowienia o Jej losie,",
+    "my, Naród Polski - wszyscy obywatele Rzeczypospolitej,",
+    "zarówno wierzący w Boga będącego źródłem prawdy, sprawiedliwości, dobra i piękna,",
+    "jak i nie podzielający tej wiary,",
+    "a te uniwersalne wartości wywodzący z innych źródeł,",
+    "równi w prawach i w powinnościach wobec dobra wspólnego - Polski,",
+    "wdzięczni naszym przodkom za ich pracę, za walkę o niepodległość okupioną ogromnymi ofiarami, za kulturę zakorzenioną w chrześcijańskim dziedzictwie Narodu i ogólnoludzkich wartościach,",
+    "nawiązując do najlepszych tradycji Pierwszej i Drugiej Rzeczypospolitej,",
+    "zobowiązani, by przekazać przyszłym pokoleniom wszystko, co cenne z ponad tysiącletniego dorobku,",
+    "złączeni więzami wspólnoty z naszymi rodakami rozsianymi po świecie,",
+    "świadomi potrzeby współpracy ze wszystkimi krajami dla dobra Rodziny Ludzkiej,",
+    "pomni gorzkich doświadczeń z czasów, gdy podstawowe wolności i prawa człowieka były w naszej Ojczyźnie łamane,",
+    "pragnąc na zawsze zagwarantować prawa obywatelskie, a działaniu instytucji publicznych zapewnić rzetelność i sprawność,",
+    "w poczuciu odpowiedzialności przed Bogiem lub przed własnym sumieniem,",
+    "ustanawiamy Konstytucję Rzeczypospolitej Polskiej",
+    "jako prawa podstawowe dla państwa",
+    "oparte na poszanowaniu wolności i sprawiedliwości, współdziałaniu władz, dialogu społecznym oraz na zasadzie pomocniczości",
+    "umacniającej uprawnienia obywateli i ich wspólnot.",
+    "Wszystkich, którzy dla dobra Trzeciej Rzeczypospolitej tę Konstytucję będą stosowali,",
+    "wzywamy, aby czynili to, dbając o zachowanie przyrodzonej godności człowieka,",
+    "jego prawa do wolności i obowiązku solidarności z innymi,",
+    "a poszanowanie tych zasad mieli za niewzruszoną podstawę Rzeczypospolitej Polskiej.",
+  ];
 </script>
 
 <svelte:head>
@@ -25,111 +51,10 @@
   class="px-5 py-5 bg-red-100 rounded-lg shadow-xl sm:py-8 md:py-10 sm:px-10 md:px-12 lg:py-12 lg:px-16"
   in:fade={{ duration: 1000 }}>
   <div
-    class="px-5 py-5 bg-white rounded-lg shadow-xl sm:py-8 md:py-10 sm:px-10 md:px-12 lg:py-12 lg:px-16"
+    class="flex flex-col space-y-6 px-5 py-5 text-base leading-snug text-center bg-white rounded-lg shadow-xl sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl sm:py-8 md:py-10 sm:px-10 md:px-12 lg:py-12 lg:px-16"
     in:fly={{ y: 100, duration: 1000 }}>
-    <p
-      class="text-base text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      W trosce o byt i przyszłość naszej Ojczyzny,
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      odzyskawszy w 1989 roku możliwość suwerennego i demokratycznego
-      stanowienia o Jej losie,
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      my, Naród Polski - wszyscy obywatele Rzeczypospolitej,
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      zarówno wierzący w Boga będącego źródłem prawdy, sprawiedliwości, dobra i
-      piękna,
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      jak i nie podzielający tej wiary,
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      a te uniwersalne wartości wywodzący z innych źródeł,
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      równi w prawach i w powinnościach wobec dobra wspólnego - Polski,
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      wdzięczni naszym przodkom za ich pracę, za walkę o niepodległość okupioną
-      ogromnymi ofiarami, za kulturę zakorzenioną w chrześcijańskim dziedzictwie
-      Narodu i ogólnoludzkich wartościach,
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      nawiązując do najlepszych tradycji Pierwszej i Drugiej Rzeczypospolitej,
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      zobowiązani, by przekazać przyszłym pokoleniom wszystko, co cenne z ponad
-      tysiącletniego dorobku,
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      złączeni więzami wspólnoty z naszymi rodakami rozsianymi po świecie,
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      świadomi potrzeby współpracy ze wszystkimi krajami dla dobra Rodziny
-      Ludzkiej,
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      pomni gorzkich doświadczeń z czasów, gdy podstawowe wolności i prawa
-      człowieka były w naszej Ojczyźnie łamane,
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      pragnąc na zawsze zagwarantować prawa obywatelskie, a działaniu instytucji
-      publicznych zapewnić rzetelność i sprawność,
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      w poczuciu odpowiedzialności przed Bogiem lub przed własnym sumieniem,
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      ustanawiamy Konstytucję Rzeczypospolitej Polskiej
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      jako prawa podstawowe dla państwa
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      oparte na poszanowaniu wolności i sprawiedliwości, współdziałaniu władz,
-      dialogu społecznym oraz na zasadzie pomocniczości
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      umacniającej uprawnienia obywateli i ich wspólnot.
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      Wszystkich, którzy dla dobra Trzeciej Rzeczypospolitej tę Konstytucję będą
-      stosowali,
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      wzywamy, aby czynili to, dbając o zachowanie przyrodzonej godności
-      człowieka,
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      jego prawa do wolności i obowiązku solidarności z innymi,
-    </p>
-    <p
-      class="pt-6 text-base leading-snug text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-      a poszanowanie tych zasad mieli za niewzruszoną podstawę Rzeczypospolitej
-      Polskiej.
-    </p>
+    {#each paragraphs as paragraph}
+      <p>{paragraph}</p>
+    {/each}
   </div>
 </div>
