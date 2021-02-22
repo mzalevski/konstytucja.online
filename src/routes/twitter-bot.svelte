b6033376c782e0511068f9b64402b176816a0d29
mzalevski <kontakt@mzalevski.pl>
2021-02-22 09:37:04 +0100

    qf

--- src/routes/twitter-bot.svelte
+++ src/routes/twitter-bot.svelte
@@ -28,41 +28,46 @@
   <meta
     name="description"
     content="Twitter Bot, który linkuje do artykułów Konstytucji RP z dnia 2
-    kwietnia 1997 r." />
+    kwietnia 1997 r."
+  />
   <meta
     name="keywords"
     content="konstytucja, informacje, bot, twitter, info, konstytucjarp,
     konstytucjaonline, online, prawo, konstytucja art, trybunał konstytucyjny,
-    sądownictwo, trybunał, prezydent, rada ministrów, sejm, senat" />
+    sądownictwo, trybunał, prezydent, rada ministrów, sejm, senat"
+  />
   <meta name="konstytucja" content="website" />
 </svelte:head>
 
-<Nav segment={'info'} />
+<Nav segment={"info"} />
 
 <h1
   class="text-lg font-thin sm:text-xl lg:text-2xl"
-  in:fly={{ x: -50, duration: 1000 }}>
+  in:fly={{ x: -50, duration: 1000 }}
+>
   Twitter Bot
 </h1>
 
 <div in:fade={{ duration: 3000 }}>
-  <div className="mt-4 text-2xl text-justify">
+  <div class="mt-4 text-2xl text-justify">
     <a
-      className="text-red-new hover:text-yellow-700"
+      class="text-red-new hover:text-yellow-700"
       style="color: blue"
       target="_blank"
-      href="https://konstytucja.link">
+      href="https://konstytucja.link"
+    >
       konstytucja.link
     </a>
     to bot, który co kilkanaście minut przeszukuje polskiego twittera, pod kątem
-    powoływania się na Konstytucję. Gdy bot znajdzie tweet, który powołuje
-    się na Konstytucję, komentuje taki tweet, podając link do odpowiedniego
-    artykułu na stronie konstytucja.online. Konto bota na tweeterze to:
+    powoływania się na Konstytucję. Gdy bot znajdzie tweet, który powołuje się na
+    Konstytucję, komentuje taki tweet, podając link do odpowiedniego artykułu na
+    stronie konstytucja.online. Konto bota na tweeterze to:
     <a
-      className="text-red-new hover:text-yellow-700"
+      class="text-red-new hover:text-yellow-700"
       style="color: blue"
       target="_blank"
-      href="https://twitter.com/_konstytucja">
+      href="https://twitter.com/_konstytucja"
+    >
       @_konstytucja
     </a>
   </div>
