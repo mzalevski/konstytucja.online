8ca918144e7737c2a4f28d1c427da321ba377c39
mzalevski <kontakt@mzalevski.pl>
2021-04-06 18:21:34 +0200

    add new mobile app info

--- src/routes/app-mobile.svelte
+++ src/routes/app-mobile.svelte
@@ -51,29 +51,28 @@
   class="text-lg font-thin sm:text-xl lg:text-2xl"
   in:fly={{ x: -50, duration: 1000 }}
 >
-  Aplikacja mobilna
+  Aplikacja mobilna (Android)
 </h1>
 
-<div in:fade={{ duration: 3000 }}>
-  <p class="mt-4 text-center text-justify">
-    <strong>Chrome</strong>
-    (i inne przeglądarki oparte na Chromium, np. Opera, Brave). Po wejściu na stronę
-    konstytucja.online na smartfonie na dole ekranu pojawi się komenda "Dodaj aplikację
-    Konstytucja do ekranu głównego". Po kliknięciu tej komendy, potwierdzamy klikając
-    "Dodaj". Konstytucja powinna pojawić się w spisie aplikacji. Aplikacja działa
-    także bez dostępu do internetu. Jeżeli część treści nie będzie się ładować w
-    trybie offline, to należy włączyć internet i przeładować stronę; przy następnym
-    podejściu te treści będą widoczne także w trybie offline.
-  </p>
-  <img class="w-full mt-6" src="images/chrome-app-guide.png" alt="" srcset="" />
-  <p class="mt-6 text-center text-justify">
-    <strong>Firefox.</strong>
-    Po wejściu na stronę konstytucja.online na smartfonie u góry ekranu pojawi się
-    ikonka domu z plusem w środku. Po kliknięciu ikonki potwierdzamy klikając "Dodaj
-    do ekranu głównego". Konstytucja powinna pojawić się w spisie aplikacji. Aplikacja
-    działa także bez dostępu do internetu. Jeżeli część treści nie będzie się ładować
-    w trybie offline, to należy włączyć internet i przeładować stronę; przy następnym
-    podejściu te treści będą widoczne także w trybie offline.
-  </p>
-  <img class="w-full mt-6" src="images/ff-app-guide.png" alt="" srcset="" />
+<div class="mt-4" in:fade={{ duration: 3000 }}>
+  <a
+    target="_blank"
+    href="https://play.google.com/store/apps/details?id=online.konstytucja.twa"
+    class="inline-flex items-center px-5 py-3 bg-gray-200 rounded-lg cursor-pointer hover:bg-gray-300 focus:outline-none"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      class="w-6 h-6"
+      viewBox="0 0 512 512"
+    >
+      <path
+        d="M99.617 8.057a50.191 50.191 0 00-38.815-6.713l230.932 230.933 74.846-74.846L99.617 8.057zM32.139 20.116c-6.441 8.563-10.148 19.077-10.148 30.199v411.358c0 11.123 3.708 21.636 10.148 30.199l235.877-235.877L32.139 20.116zM464.261 212.087l-67.266-37.637-81.544 81.544 81.548 81.548 67.273-37.64c16.117-9.03 25.738-25.442 25.738-43.908s-9.621-34.877-25.749-43.907zM291.733 279.711L60.815 510.629c3.786.891 7.639 1.371 11.492 1.371a50.275 50.275 0 0027.31-8.07l266.965-149.372-74.849-74.847z"
+      />
+    </svg>
+    <span class="flex flex-col items-start ml-4 leading-none">
+      <span class="mb-1 text-xs text-gray-600">Pobierz z</span>
+      <span class="font-medium">Google Play</span>
+    </span>
+  </a>
 </div>
