53d5e7f675007fd5aa9316fbc538eccc05490263
mzalevski <kontakt@mzalevski.pl>
2021-09-06 17:57:14 +0200

    add Key component & btr skroty styles

---
+++ src/components/Key.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let small = false;
+</script>
+
+<div
+  class={`flex flex-col justify-center ${
+    small ? "min-h-4 sm:h-10 inline-flex px-3" : "min-h-10 sm:h-16 px-6"
+  } text-2xl font-extrabold text-center text-gray-800 bg-gray-100 border rounded-lg shadow-xl cursor-default hover:shadow-inner transition`}
+>
+  <slot />
+</div>
--- src/routes/skroty.svelte
+++ src/routes/skroty.svelte
@@ -5,6 +5,7 @@
   import { EventManager } from "mjolnir.js";
   import { goto, prefetch } from "@sapper/app";
   import { isMobile } from "./_helpers";
+  import Key from "../components/Key.svelte";
 
   let eventManager;
   let showDropdown = false;
@@ -58,152 +59,134 @@
   Skróty klawiszowe
 </h1>
 
-<div class="mt-4" in:fade={{ duration: 3000 }}>
-  <h2 class="mt-4 text-xl font-thin">Wszędzie</h2>
-  <div class="flex text-lg sm:space-x-4">
-    <img class="w-10 h-10 sm:w-16 sm:h-16" src="/images/keys/D.png" alt="" />
-    <span
-      class="pl-2 pt-2 sm:pt-3 text-sm sm:text-base sm:w-7/12 hover:text-red-new"
-    >
-      zmiana trybu tryb ciemny / tryb jasny
-    </span>
-    <span
-      class="hidden md:block pt-2 sm:pt-3 text-sm sm:text-base sm:w-4/12
-      hover:text-red-new"
-    >
-      (D)arkmode (en. ciemny tryb)
-    </span>
+<div class="mt-4 flex flex-col space-y-4" in:fade={{ duration: 3000 }}>
+  <h2 class="pt-4 text-xl font-thin">Wszędzie</h2>
+
+  <div class="flex text-lg sm:space-x-4 border rounded p-4">
+    <Key>D</Key>
+
+    <div class="h-16 flex flex-col justify-center">
+      <span class="p-2 pt-2 sm:pt-3 text-base sm:text-lg">
+        Zmiana trybu tryb ciemny / tryb jasny
+      </span>
+    </div>
   </div>
 
-  <h2 class="mt-4 text-xl font-thin">Strona główna</h2>
-  <div class="flex text-lg sm:space-x-4">
-    <img
-      class="w-10 h-10 sm:w-16 sm:h-16"
-      src="/images/keys/slash.png"
-      alt=""
-    />
-    <span
-      class="pl-2 pt-2 sm:pt-3 text-sm sm:text-base sm:w-7/12 hover:text-red-new"
-    >
-      przejdź do wyszukiwania tekstowego
-    </span>
-    <span class="hidden md:block pt-2 sm:w-4/12 hover:text-red-new">
-      ten sam klawisz co
-      <img class="w-10 h-10 inline" src="/images/keys/question.png" alt="" />
-    </span>
+  <h2 class="pt-4 text-xl font-thin">Strona główna</h2>
+
+  <div class="flex text-lg sm:space-x-4 border rounded p-4">
+    <Key>/</Key>
+
+    <div class="h-16 flex flex-col justify-center">
+      <span class="p-2 pt-2 sm:pt-3 text-base sm:text-lg">
+        Przejdź do wyszukiwania tekstowego. ten sam klawisz co <span
+          class="inline-block w-2"
+        /><Key small>?</Key>
+      </span>
+    </div>
   </div>
 
-  <h2 class="mt-4 text-xl font-thin">Podstrona artykułu</h2>
+  <h2 class="pt-4 text-xl font-thin">Podstrona artykułu</h2>
 
-  <div class="flex text-lg sm:space-x-4">
-    <img
-      class="w-10 h-10 sm:w-16 sm:h-16"
-      src="/images/keys/right.png"
-      alt=""
-    />
-    <span
-      class="pl-2 pt-2 sm:pt-3 text-sm sm:text-base sm:w-7/12 hover:text-red-new"
+  <div class="flex text-lg sm:space-x-4 border rounded p-4">
+    <Key
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M14 5l7 7m0 0l-7 7m7-7H3"
+        />
+      </svg></Key
     >
-      przejdź do następnego artykułu
-    </span>
-    <span
-      class="hidden md:block pt-2 sm:pt-3 text-sm sm:text-base sm:w-4/12
-      hover:text-red-new"
-    >
-      strzałka w prawo (przytrzymaj, aby przejść o więcej artykułów)
-    </span>
+
+    <div class="h-16 flex flex-col justify-center">
+      <span class="p-2 pt-2 sm:pt-3 text-base sm:text-lg">
+        Przejdź do następnego artykułu (przytrzymaj, aby przejść o więcej
+        artykułów)
+      </span>
+    </div>
   </div>
 
-  <div class="flex text-lg sm:space-x-4">
-    <img class="w-10 h-10 sm:w-16 sm:h-16" src="/images/keys/left.png" alt="" />
-    <span
-      class="pl-2 pt-2 sm:pt-3 text-sm sm:text-base sm:w-7/12 hover:text-red-new"
+  <div class="flex text-lg sm:space-x-4 border rounded p-4">
+    <Key
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M10 19l-7-7m0 0l7-7m-7 7h18"
+        />
+      </svg></Key
     >
-      przejdź do poprzedniego artykułu
-    </span>
-    <span
-      class="hidden md:block pt-2 sm:pt-3 text-sm sm:text-base sm:w-4/12
-      hover:text-red-new"
-    >
-      strzałka w lewo (przytrzymaj, aby przejść o więcej artykułów)
-    </span>
+
+    <div class="h-16 flex flex-col justify-center">
+      <span class="p-2 pt-2 sm:pt-3 text-base sm:text-lg">
+        Przejdź do poprzedniego artykułu (przytrzymaj, aby przejść o więcej
+        artykułów)
+      </span>
+    </div>
   </div>
 
-  <div class="flex text-lg sm:space-x-4">
-    <img class="w-10 h-10 sm:w-16 sm:h-16" src="/images/keys/F.png" alt="" />
-    <span
-      class="pl-2 pt-2 sm:pt-3 text-sm sm:text-base sm:w-7/12 hover:text-red-new"
-    >
-      szybko przejdź do innego artykułu
-    </span>
-    <span
-      class="hidden md:block pt-2 sm:pt-3 text-sm sm:text-base sm:w-4/12
-      hover:text-red-new"
-    >
-      (F)ind (en. znajdź)
-    </span>
+  <div class="flex text-lg sm:space-x-4 border rounded p-4">
+    <Key>F</Key>
+
+    <div class="h-16 flex flex-col justify-center">
+      <span class="p-2 pt-2 sm:pt-3 text-base sm:text-lg">
+        Szybko przejdź do innego artykułu
+      </span>
+    </div>
   </div>
 
-  <div class="flex text-lg sm:space-x-4">
-    <img class="w-10 h-10 sm:w-16 sm:h-16" src="/images/keys/Q.png" alt="" />
-    <span
-      class="pl-2 pt-2 sm:pt-3 text-sm sm:text-base sm:w-7/12 hover:text-red-new"
-    >
-      pokaż / schowaj dyskusję dot. artykułem
-    </span>
-    <span
-      class="hidden md:block pt-2 sm:pt-3 text-sm sm:text-base sm:w-4/12
-      hover:text-red-new"
-    >
-      (Q)&A (en. pytania i odpowiedzi)
-    </span>
+  <div class="flex text-lg sm:space-x-4 border rounded p-4">
+    <Key>Q</Key>
+
+    <div class="h-16 flex flex-col justify-center">
+      <span class="p-2 pt-2 sm:pt-3 text-base sm:text-lg">
+        Pokaż / schowaj dyskusję dot. artykułem
+      </span>
+    </div>
   </div>
 
-  <div class="flex text-lg sm:space-x-4">
-    <img class="w-10 h-10 sm:w-16 sm:h-16" src="/images/keys/W.png" alt="" />
-    <span
-      class="pl-2 pt-2 sm:pt-3 text-sm sm:text-base sm:w-7/12 hover:text-red-new"
-    >
-      pokaż / schowaj wyjaśnienie artykułu
-    </span>
-    <span
-      class="hidden md:block pt-2 sm:pt-3 text-sm sm:text-base sm:w-4/12
-      hover:text-red-new"
-    >
-      (W)yjaśnienie
-    </span>
+  <div class="flex text-lg sm:space-x-4 border rounded p-4">
+    <Key>W</Key>
+
+    <div class="h-16 flex flex-col justify-center">
+      <span class="p-2 pt-2 sm:pt-3 text-base sm:text-lg">
+        Pokaż / schowaj wyjaśnienie artykułu
+      </span>
+    </div>
   </div>
 
-  <h2 class="mt-4 text-xl font-thin">Ogólne</h2>
-  <div class="flex text-lg sm:space-x-4">
-    <img
-      class="w-10 h-10 sm:w-16 sm:h-16"
-      src="/images/keys/enter.png"
-      alt=""
-    />
-    <span
-      class="pl-2 pt-2 sm:pt-3 text-sm sm:text-base sm:w-7/12 hover:text-red-new"
-    >
-      przejdź dalej / zaakceptuj
-    </span>
-    <span
-      class="pt-2 sm:pt-3 text-sm sm:text-base sm:w-4/12 hover:text-red-new"
-    />
+  <h2 class="pt-4 text-xl font-thin">Ogólne</h2>
+
+  <div class="flex text-lg sm:space-x-4 border rounded p-4">
+    <Key>enter</Key>
+
+    <div class="h-16 flex flex-col justify-center">
+      <span class="p-2 pt-2 sm:pt-3 text-base sm:text-lg">
+        Przejdź dalej / zaakceptuj
+      </span>
+    </div>
   </div>
 
-  <div class="flex text-lg sm:space-x-4">
-    <img
-      class="w-10 h-10 sm:w-16 sm:h-16"
-      src="/images/keys/escape.png"
-      alt=""
-    />
-    <span
-      class="pl-2 pt-2 sm:pt-3 text-sm sm:text-base sm:w-7/12 hover:text-red-new"
-    >
-      powrót / anuluj
-    </span>
-    <span
-      class="pt-2 sm:pt-3 text-sm sm:text-base sm:w-4/12 hover:text-red-new"
-    />
+  <div class="flex text-lg sm:space-x-4 border rounded p-4">
+    <Key>esc</Key>
+
+    <div class="h-16 flex flex-col justify-center">
+      <span class="p-2 pt-2 sm:pt-3 text-base sm:text-lg">powrót / anuluj</span>
+    </div>
   </div>
 </div>
